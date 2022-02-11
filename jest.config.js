cd195a10c42e4fece86ac171bf30d25441fabb11
MrPluto0 <710852553@qq.com>
2022-02-12 00:38:46 +0800

    control and declare

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  clearMocks: true,
+
+  collectCoverage: true,
+
+  coverageDirectory: "coverage",
+
+  testMatch: ["<rootDir>/test/*.test.js"],
+};
